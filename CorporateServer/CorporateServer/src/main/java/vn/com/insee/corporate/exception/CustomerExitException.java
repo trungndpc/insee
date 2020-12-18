0b85855d53005a6a5c4ccbe72a7b33a99469e552
trungnd2 <trungnd2@vng.com.vn>
2020-12-18 10:02:33 +0700

    delete server

--- CorporateServer/CorporateServer/src/main/java/vn/com/insee/corporate/exception/CustomerExitException.java
+++
@@ -1,8 +0,0 @@
-package vn.com.insee.corporate.exception;
-
-public class CustomerExitException extends Exception{
-
-    public CustomerExitException() {
-        super("phone is registered");
-    }
-}
