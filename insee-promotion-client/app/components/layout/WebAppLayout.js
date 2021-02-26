63b9e904512fe318192bc3e8fd1d3220ff885690
trungnd2 <trungnd2@vng.com.vn>
2021-02-26 19:56:49 +0700

    update

--- insee-promotion-client/app/components/layout/WebAppLayout.js
+++ insee-promotion-client/app/components/layout/WebAppLayout.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
-// import '../../resources/webapp/css/main.css'
+import '../../resources/webapp/css/me.css';
 import '../../resources/webapp/css/style.css';
 import '../../resources/webapp/css/responsive.css';
-import '../../resources/webapp/css/me.css';
 import '../../resources/css/mobile/font-awesome.css'
-import {ContentSideBar} from '../layout/SideBar'
-import {City} from '../../data/Location'
+import { ContentSideBar } from '../layout/SideBar'
+import { City } from '../../data/Location'
 
 class WebAppLayout extends Component {
   constructor(props) {
@@ -40,43 +39,41 @@ class WebAppLayout extends Component {
     const certificate = contractor && contractor.volumeCiment > 700
     return (
       <div className="theme-layout">
-        <section>
+        <section style={{ zIndex: 50 }}>
           <div className="feature-photo">
             <figure><img className="responsive" style={{ objectFit: 'cover' }} src={require('../../resources/images/banner.png')} alt="" /></figure>
             <div className="container-fluid">
               <div className="row merged">
-                <div className="col-lg-2 col-4 col-avatar">
+                <div className="col-avatar">
                   <div className="user-avatar">
                     <figure>
                       <img src={account && account.avatar} alt="" />
                     </figure>
                   </div>
                 </div>
-                <div className="col-lg-10 col-8">
+                <div className="col-name">
                   <div className="timeline-info">
                     <ul>
                       <li className="admin-name">
                         <h5>{account && account.name}</h5>
-                        {contractor && <span>{City.getName(contractor.mainAreaId)}</span> }
-                        {/* {certificate && <img className="ntx-icon" src="https://insee-promotion-vn.s3.us-east-2.amazonaws.com/static/images/certificate-icon.png"/> } */}
+                        {contractor && <span>{City.getName(contractor.mainAreaId)}</span>}
                       </li>
                     </ul>
                   </div>
+                </div>
+                <div className="col-menu">
                   <i onClick={this._toggleMenuBar} className="fa fa-bars icon-bar " aria-hidden="true"></i>
                 </div>
               </div>
             </div>
             <div>
             </div>
-            {
-              this.state.isShowMobileBar &&
-              <div id="main-menu" className={`topnav navbar collapse navbar-collapse in`}>
-                <ContentSideBar onClickToLink={this._onClickToLink}/>
-              </div>
-            }
           </div>
         </section>
-        {this.props.children}
+        <div id="main-menu" className={`topnav  ${this.state.isShowMobileBar ? 'showmenu' : ''}`}>
+          <ContentSideBar onClickToLink={this._onClickToLink} />
+          {this.props.children}
+        </div>
       </div>
     )
   }
--- insee-promotion-client/webpack.config.dev.js
+++ insee-promotion-client/webpack.config.dev.js
@@ -5,136 +5,31 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const config = require('./app/config/development');
 
-const CSSModuleLoader = {
-  loader: 'css-loader',
-  options: {
-    modules: true,
-    localIdentName: '[local]__[hash:base64:8]'
-  }
-}
-
-const CSSLoader = {
-  loader: 'css-loader',
-  options: {
-    modules: false
-  }
-}
-
-const postCSSLoader = {
-  loader: 'postcss-loader',
-  options: {
-    ident: 'postcss',
-    sourceMap: true
-  }
-}
-
 module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '.css', '.png', '.jpg', '.gif', '.jpeg']
   },
   mode: 'development',
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'eval-source-map',
   entry: [
+    'webpack-hot-middleware/client?reload=true',
     path.join(__dirname, "app", "index.js")
   ],
   output: {
     path: path.join(__dirname, "build"),
-    filename: "main.js",
+    filename: "bundle.js",
     publicPath: "/"
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-          filename: 'vender.bundle.js'
-        }
-      }
-    }
   },
   module: {
     rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
+      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel-loader'] },
       { test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file-loader' },
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
       { test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' },
+      { test: /\.(jpe?g|png|gif)$/i, loader: 'file-loader?name=images/[name].[ext]' },
       { test: /\.ico$/, loader: 'file-loader?name=[name].[ext]' },
-      {
-        test: /\.scss$/,
-        exclude: /\.module\.scss$/,
-        use: ['style-loader', CSSLoader, postCSSLoader, 'sass-loader']
-      },
-      {
-        test: /\.module\.scss$/,
-        use: [
-          'style-loader',
-          CSSModuleLoader,
-          postCSSLoader,
-          'sass-loader',
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          require.resolve('style-loader'),
-          {
-            loader: require.resolve('css-loader'),
-            options: {
-              importLoaders: 1
-            }
-          },
-          {
-            loader: require.resolve('postcss-loader'),
-            options: {
-              // Necessary for external CSS imports to work
-              // https://github.com/facebookincubator/create-react-app/issues/2677
-              ident: 'postcss',
-              plugins: () => [
-                require('postcss-flexbugs-fixes'),
-                autoprefixer({
-                  overrideBrowserslist: [
-                    '>1%',
-                    'last 4 versions',
-                    'Firefox ESR',
-                    'not ie < 9' // React doesn't support IE8 anyway
-                  ],
-                  flexbox: 'no-2009'
-                })
-              ]
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[path][name]-[hash:8].[ext]"
-            }
-          }
-        ]
-      },
-      {
-        test: /\.mp4|wav$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[path][name]-[hash:8].[ext]"
-            }
-          }
-        ]
-      },
+      { test: /(\.css|\.scss|\.sass)$/, loaders: ['style-loader', 'css-loader?sourceMap'] }
     ]
   },
   plugins: [
@@ -146,10 +41,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: path.join(__dirname, "public", "index.ejs")
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
     })
   ],
   watch: false,
@@ -166,4 +57,4 @@ module.exports = {
     historyApiFallback: true,
     disableHostCheck: true
   }
-};
+}
