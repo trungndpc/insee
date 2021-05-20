12e979ec1c695d2eb0d74b23de267280d557820e
trungnd <trungnd.pc@gmail.com>
2021-05-20 15:30:53 +0700

    add name cement

--- insee-promotion-client/webpack.config.prod.js
+++ insee-promotion-client/webpack.config.prod.js
@@ -6,7 +6,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const config = require('./app/config/production');
-const version = "1.1.7";
+const version = "1.1.8";
 const public_static = "https://nhathau.insee.com.vn/static/";
 
 const CSSModuleLoader = {
