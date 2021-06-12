e763e3712aff9bb1c541505906e4f9d0b44db7e7
trungnd2 <trungnd.pc@gmail.com.vn>
2021-06-12 11:28:12 +0700

    live

---
+++ insee-promotion-admin-client/match.sql
@@ -0,0 +1,40 @@
+
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E1', 1006, 1004, '2021-06-12 02:00:00', '2021-06-12 04:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E2', 1007, 1005, '2021-06-12 20:00:00', '2021-06-12 22:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E3', 1009, 1010, '2021-06-12 23:00:00', '2021-06-13 01:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E4', 1008, 1011, '2021-06-13 02:00:00', '2021-06-13 04:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E5', 1018, 1016, '2021-06-13 20:00:00', '2021-06-13 22:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E6', 1012, 1014, '2021-06-13 23:00:00', '2021-06-14 01:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E7', 1013, 1015, '2021-06-14 02:00:00', '2021-06-14 02:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E8', 1019, 1017, '2021-06-14 20:00:00', '2021-06-14 22:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E9', 1020, 1021, '2021-06-14 23:00:00', '2021-06-15 01:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E10', 1022, 1023, '2021-06-15 02:00:00', '2021-06-15 04:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E11', 1026, 1027, '2021-06-15 23:00:00', '2021-06-16 01:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E12', 1024, 1025, '2021-06-16 02:00:00', '2021-06-16 04:00:00', 1, 2 );
+
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E13', 1010, 1011, '2021-06-16 20:00:00', '2021-06-16 22:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E14', 1006, 1007, '2021-06-16 23:00:00', '2021-06-17 01:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E15', 1004, 1005, '2021-06-17 02:00:00', '2021-06-17 04:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E16', 1015, 1014, '2021-06-17 20:00:00', '2021-06-17 22:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E17', 1009, 1008, '2021-06-17 23:00:00', '2021-06-18 01:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E18', 1013, 1012, '2021-06-18 02:00:00', '2021-06-18 04:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E19', 1023, 1021, '2021-06-18 20:00:00', '2021-06-18 22:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E20', 1016, 1017, '2021-06-18 23:00:00', '2021-06-19 01:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E21', 1018, 1019, '2021-06-19 02:00:00', '2021-06-19 04:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E22', 1026, 1024, '2021-06-19 20:00:00', '2021-06-19 22:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E23', 1027, 1025, '2021-06-19 23:00:00', '2021-06-20 01:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E24', 1022, 1020, '2021-06-20 02:00:00', '2021-06-20 04:00:00', 1, 2 );
+
+
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E25', 1004, 1007, '2021-06-20 23:00:00', '2021-06-21 01:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E26', 1005, 1006, '2021-06-20 23:00:00', '2021-06-21 01:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E27', 1015, 1012, '2021-06-21 23:00:00', '2021-06-22 01:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E28', 1014, 1013, '2021-06-21 23:00:00', '2021-06-22 01:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E29', 1011, 1009, '2021-06-22 02:00:00', '2021-06-22 04:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E30', 1010, 1008, '2021-06-22 02:00:00', '2021-06-22 04:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E31', 1016, 1019, '2021-06-23 02:00:00', '2021-06-23 04:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E32', 1017, 1018, '2021-06-23 02:00:00', '2021-06-23 04:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E33', 1023, 1020, '2021-06-23 23:00:00', '2021-06-24 01:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E34', 1021, 1022, '2021-06-23 23:00:00', '2021-06-24 01:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E35', 1027, 1024, '2021-06-24 02:00:00', '2021-06-24 04:00:00', 1, 2 );
+INSERT INTO promotion.match(code, team_one, team_two, time_start, time_end, status, season_id) VALUES('E36', 1025, 1026, '2021-06-24 02:00:00', '2021-06-24 04:00:00', 1, 2 );
