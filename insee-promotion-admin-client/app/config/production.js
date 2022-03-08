2f646624c29321ca1f8055ccf69d2bfb67571c0d
trung <trungnd.pc>
2022-03-08 07:35:46 +0700

    update

--- insee-promotion-admin-client/app/config/production.js
+++ insee-promotion-admin-client/app/config/production.js
@@ -2,6 +2,6 @@ export default {
   __DEV__: true,
   'process.env': {
     NODE_ENV: JSON.stringify('production'),
-    DOMAIN: JSON.stringify('http://admin.nhathau.insee.com.vn')
+    DOMAIN: JSON.stringify('https://admin-nhathau.insee.udev.com.vn')
   }
 }
--- insee-promotion-admin-client/deploy.sh
+++ insee-promotion-admin-client/deploy.sh
@@ -2,4 +2,4 @@
 local_dir=./
 server=stackops@61.28.229.63
 
-rsync -a $local_dir/dist/ $server:/var/www/admin.nhathau.insee.com.vn/html
+rsync -a $local_dir/dist/ $server:/var/www/admin-nhathau.insee.udev.com.vn/html
